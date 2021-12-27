4294b2afef8407fb51e34a0676f30af2a24f5a0a
onurgunayX <onurgunay.ytu@gmail.com>
2021-12-27 15:36:10 +0300

    Extra commit

--- src/test/java/Day03Work/Work03.java
+++ src/test/java/Day03Work/Work03.java
@@ -3,5 +3,6 @@ package Day03Work;
 public class Work03 {
     public static void main(String[] args) {
         System.out.println("Bugün Muhammet abiyle tekrardayız");
+        System.out.println("bu da extra");
     }
 }
